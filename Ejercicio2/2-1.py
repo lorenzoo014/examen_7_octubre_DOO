f7af6e614f294bb29fca12f2fc1571e12dff9d30
lorenzoo22 <lagudmarr@gmail.com>
2022-10-07 19:14:28 +0200

    Ejercicio_2_acabado

--- Ejercicio2/2-1.py
+++ Ejercicio2/2-1.py
@@ -1,16 +1,28 @@
 l=[18, 50, 210, 80, 145, 333, 70, 30]
 
-#hacemos un bucle para recorrerlo y mostrar lo que nos dicen
+lista_aux = []
 
-#def imprimir(lista):
 
-newlist = [x for x in l if x%10==0 and x<200]
-print(newlist)
+def seleccionar(diccionario):
+    for element in l:
+        if(element%10 ==0 and element<200):
+            lista_aux.append(element)
+        elif(element>300):
+            break
+        return lista_aux
+print(seleccionar(l))
 
-for x in l:
-    if(x>300):
-        print(exit)
-        exit()
+# #hacemos un bucle para recorrerlo y mostrar lo que nos dicen
+
+# #def imprimir(lista):
+
+# newlist = [x for x in l if x%10==0 and x<200]
+# print(newlist)
+
+# for x in l:
+#     if(x>300):
+#         print(exit)
+#         exit()
 
 
 
