7d3eb99f9fccb4213dce3a88155b759607168c59
lorenzoo22 <lagudmarr@gmail.com>
2022-10-07 18:09:45 +0200

    1er_punto_completado

--- Ejercicio_1/1_1.py
+++ Ejercicio_1/1_1.py
@@ -1,4 +1,5 @@
 
+import copy
 #-----------Ejercicio_1.1-----------#
 lista = []
 tupla = (1,2,3)
@@ -13,3 +14,18 @@ posicion=-1
 for i in range(2,5,2):
     print(lista[posicion])
     posicion = posicion-i
+
+
+#-----------Ejercicio_1.3-----------#
+elemento_ultimo = lista[-1]
+lista[-1]=lista[0]
+lista[0]=elemento_ultimo
+print(lista)
+
+#-----------Ejercicio_1.4-----------#
+lista.pop()
+print(lista)
+
+#-----------Ejercicio_1.5-----------#
+lista2 = lista.copy()
+
