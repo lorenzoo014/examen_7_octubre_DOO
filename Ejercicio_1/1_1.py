153bb524a9e3fcf052008e9d692ef3d8fb585a9a
lorenzoo22 <lagudmarr@gmail.com>
2022-10-07 18:19:06 +0200

    Ejercicio-1_1 completado

--- Ejercicio_1/1_1.py
+++ Ejercicio_1/1_1.py
@@ -1,31 +1,43 @@
-
 import copy
+class Apartado_1():
+
 #-----------Ejercicio_1.1-----------#
-lista = []
-tupla = (1,2,3)
-lista.append(5)
-lista.append("hola")
-lista.append(True)
-lista.append(tupla)
-print(lista)
+    @staticmethod
+    def elementos(lista=[]):
+        tupla = (1,2,3)
+        lista.append(5)
+        lista.append("hola")
+        lista.append(True)
+        lista.append(tupla)
+        return lista
 
 #-----------Ejercicio_1.2-----------#
-posicion=-1
-for i in range(2,5,2):
-    print(lista[posicion])
-    posicion = posicion-i
+    @staticmethod
+    def cada_dos(lista=[]):
+        posicion=-1
+        for i in range(2,5,2):
+            lista2 =[]
+            lista2.append(lista[posicion])
+            posicion = posicion-i
+        return lista2
 
 
 #-----------Ejercicio_1.3-----------#
-elemento_ultimo = lista[-1]
-lista[-1]=lista[0]
-lista[0]=elemento_ultimo
-print(lista)
+    @staticmethod
+    def cambia_elementos(lista=[]):   
+        elemento_ultimo = lista[-1]
+        lista[-1]=lista[0]
+        lista[0]=elemento_ultimo
+        return lista
 
 #-----------Ejercicio_1.4-----------#
-lista.pop()
-print(lista)
+    @staticmethod
+    def quita_ultimo(lista=[]):
+        lista.pop()
+        return lista
 
 #-----------Ejercicio_1.5-----------#
-lista2 = lista.copy()
+    @staticmethod
+    def copia(lista=[]):
+        lista2 = lista.copy()
 
