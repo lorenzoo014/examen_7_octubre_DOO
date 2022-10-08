6014671b267e1b791b201cbc2dcebfbe3d06c70c
lorenzoo22 <lagudmarr@gmail.com>
2022-10-08 13:35:50 +0200

    beta Ejercicio_3_1

---
+++ Ejercicio3/3_1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+#Ejercicio_1
+np.random.seed(36)
+matriz = np.random.randint(0,10,(6,6))
+print(matriz)
+
+#Ejercicio_2
+print(matriz.nbytes)
+
+#Ejercicio_3
+
+#desv. tipica
+des_tipica = np.std(matriz)
+media = np.mean(matriz)
+print(des_tipica,media)
+#Ejercicio_4
+np.random.seed(36)
+matriz_2 = np.random.normal(media, des_tipica, (6,6))
+print(matriz_2)
+matriz_2 = matriz_2.round()
+print(matriz_2)
+
+
