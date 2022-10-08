3a5d10375ad109bd178ed1fec595a0a8cfd93b78
Rauulito <7rnavarro7@gmail.com>
2022-10-08 12:44:51 +0200

    cambio de nombre ejercicio 2

---
+++ Ejercicio2/2_2.py
@@ -0,0 +1,14 @@
+
+
+#listado de cadenas de texto
+cadena=["PEP 8", "PEP 248", "PEP 257"]
+
+#funcion que devuelve el numero total de caracteres del listado de la cadenas de texto
+
+
+contador=0
+for x in cadena:
+    len(x)
+    contador=contador+len(x)
+
+print(contador)
