20448365df314d2a6686b87fbd457ec70d5acc4d
Rauulito <7rnavarro7@gmail.com>
2022-10-08 13:40:43 +0200

    cambiado

--- Ejercicio3/3_2.py
+++ Ejercicio3/3_2.py
@@ -8,8 +8,8 @@ array = np.random.randint(100 , size=(3, 4))
 
 #3.2.2
 for i in array:
-    if(i>0 and i%5==0):
-        array[i]=i
+    if(array[i]>0 and array[i]%5==0):
+        array[i]=[i]
     else:
         array[i]=-1
 print(array)
