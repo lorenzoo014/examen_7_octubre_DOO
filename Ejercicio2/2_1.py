ff1c5c0a6e04fad84d79a5c872699ab7cdc72b81
lorenzoo22 <lagudmarr@gmail.com>
2022-10-08 12:49:41 +0200

    redundancia

--- Ejercicio2/2_1.py
+++ Ejercicio2/2_1.py
@@ -1,32 +1,14 @@
+
 l=[18, 50, 210, 80, 145, 333, 70, 30]
 
 lista_aux = []
 
 
 def seleccionar(diccionario):
-    for element in l:
+    for element in diccionario:
         if(element%10 ==0 and element<200):
             lista_aux.append(element)
         elif(element>300):
             break
         return lista_aux
 print(seleccionar(l))
-
-# #hacemos un bucle para recorrerlo y mostrar lo que nos dicen
-
-# #def imprimir(lista):
-
-# newlist = [x for x in l if x%10==0 and x<200]
-# print(newlist)
-
-# for x in l:
-#     if(x>300):
-#         print(exit)
-#         exit()
-
-
-
-
-
-
-
