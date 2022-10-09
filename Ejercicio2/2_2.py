ecaa248d9b7e6b1fe065669855a8f6d71f3bd9a1
lorenzoo22 <lagudmarr@gmail.com>
2022-10-09 18:56:24 +0200

    Pequeño_cambio

--- Ejercicio2/2_2.py
+++ Ejercicio2/2_2.py
@@ -6,7 +6,7 @@ c=["PEP 8", "PEP 248", "PEP 257"]
 #funcion que devuelve el numero total de caracteres del listado de la cadenas de texto
 def totalcaracteres(cadena):
     contador=0 #definimos un contador
-    for x in c:
+    for x in cadena:
         len(x)
         contador=contador+len(x)
     return contador
