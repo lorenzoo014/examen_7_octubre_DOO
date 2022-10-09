b37e49b3c4e3b64ff050d135d16dde99dad6407d
Rauulito <7rnavarro7@gmail.com>
2022-10-09 19:52:19 +0200

    hecho

--- Ejercicio4/Apartado3.py
+++ Ejercicio4/Apartado3.py
@@ -1,6 +1,6 @@
 # importamos la las librerias que necesitaremos
 import pandas as pd
-import numpy as np
+
 
 # importamos los datos
 # pd.read_csv('auto-mpg.data',delim_whitespace=True,header =None)
